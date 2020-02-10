e5df67d5f26b7ae5f798704cda379e5b9eff4caf
Yogrishi Mulliah <yogrishi.mulliah1@umail.uom.ac.mu>
2020-02-10 19:48:04 +0400

    First commit of Display Project

---
+++ src/Display.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Display 
+{
+
+	public static void main(String[] args) 
+	{
+		Date date = new java.util.Date();
+		System.out.println("Today's date is : " + date);
+	}
+
+}
